7aaf215df81ce53f7cbca99e6394f315accd1128
Emily <johnsemi@gmail.com>
2014-04-13 22:19:50 -0400

    production asset compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Gpna::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   config.assets.precompile += %w( member_functions.js )
 
