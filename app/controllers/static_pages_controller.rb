04dadc92bed0a7f06cf0b6dbdfff9002a61fabdb
Emily <johnsemi@gmail.com>
2014-04-14 17:25:22 -0400

    css

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,8 @@ class StaticPagesController < ApplicationController
   def home
   	 @totalMembers = Member.count
 
-     @newMembers = Member.find(:all, :order => 'joindate, id', :limit => 10)
-  	 
+     #@newMembers = Member.find(:all, :order => 'joindate', :limit => 10)
+  	 @newMembers = Member.order(joindate: :desc).limit(10)
 
 
 	#@donations = Donation.group("donationdate").select("DATE_FORMAT(donationdate, '%Y-%m') AS d_month, sum(amount) AS d_total")
@@ -15,7 +15,7 @@ class StaticPagesController < ApplicationController
   end
 
   def reports
-  	     
+
 	@donations = Donation.find(:all, :order => 'donationdate')
 	@monthly_donations = @donations.group_by(&:month)
 
